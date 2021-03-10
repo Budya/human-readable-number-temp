d8d36ae321adef12e3101521c1579479075415ba
Dima <budya2012@gmail.com>
2021-03-11 00:37:38 +0300

    fead: implementing done

--- src/draft.js
+++ src/draft.js
@@ -1,28 +1,58 @@
-function toReadable (number) {
+function toReadable(number) {
     let numberString = number.toString();
-    let numberStringArrayReversed = numberString.split('').reverse().join('');
-    let hundreds = ['','one hundred', 'two hundred', 'three hundred',
-                    'fout hundred', 'five hundred', 'six hundred',
-                    'seven hundred', 'eight hundred' , 'nine hundred'];
-    let tens = ['', 'ten','eleven','twelve','thirteen','fourteen',
-                'fifteen','sixteen','seventeen','nineteen']
-    let tensMore = ['', '', 'twenty', 'thirty', 'forty', 'fifty',
-                    'sixty', 'seventy', 'eighty', 'ninety']
-    let ones = ['zero', 'one', 'two','three', 'four', 'five',
-                'six', 'seven', 'eight', 'nine']
-    
+    let numberStringTemp = numberString;
     let readableNumber = '';
-    let firstDigit = 0;
-    let secondDigit = 0;
-    let thirdDigit = 0;
-    
 
-    console.log(numberStringArrayReversed)
+    let ones = ['', 'one', 'two', 'three', 'four', 'five',
+        'six', 'seven', 'eight', 'nine'
+    ];
 
-    //go home
+    let ten = ['', 'ten'];
 
-    
-  
+    let tens = ['','eleven', 'twelve', 'thirteen', 'fourteen',
+        'fifteen', 'sixteen', 'seventeen', 'eighteen' , 'nineteen'
+    ];
+
+    let tensMore = ['','', 'twenty', 'thirty', 'forty', 'fifty',
+        'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    let hundreds = ['', 'one hundred', 'two hundred', 'three hundred',
+        'four hundred', 'five hundred', 'six hundred',
+        'seven hundred', 'eight hundred', 'nine hundred'
+    ];
+
+    if (number === 0) return readableNumber = 'zero';
+
+    if (numberStringTemp.length === 3) {
+        readableNumber += hundreds[numberStringTemp[0]];
+        numberStringTemp = numberStringTemp.slice(1)
+        if(numberStringTemp[0] == 0) numberStringTemp = numberStringTemp.slice(1);       
+    }
+
+    if (numberStringTemp.length === 2 && numberStringTemp == 10) {
+        
+        readableNumber = readableNumber + ' ' + ten[numberStringTemp[0]];
+        numberStringTemp = '';
+    }
+
+    if (numberStringTemp.length === 2 && numberStringTemp >= 11 && numberStringTemp < 20) {
+        readableNumber = readableNumber + ' ' + tens[numberStringTemp[1]];
+        numberStringTemp = '';
+    }
+
+    if (numberStringTemp.length === 2 && numberStringTemp >= 20 && numberStringTemp < 100) {
+        readableNumber = readableNumber + ' ' + tensMore[numberStringTemp[0]];
+        numberStringTemp = numberStringTemp.slice(1);
+    }
+
+    if (numberStringTemp.length === 1 && numberStringTemp >= 1 && numberStringTemp < 10) {
+        readableNumber = readableNumber + ' ' + ones[numberStringTemp[0]];
+        numberStringTemp = '';
+    }
+    console.log(readableNumber)
+
+    return readableNumber.trim();
 }
 
-toReadable(123)
+toReadable(909)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let numberString = number.toString();
+    let numberStringTemp = numberString;
+    let readableNumber = '';
+
+    let ones = ['', 'one', 'two', 'three', 'four', 'five',
+        'six', 'seven', 'eight', 'nine'
+    ];
+
+    let ten = ['', 'ten'];
+
+    let tens = ['','eleven', 'twelve', 'thirteen', 'fourteen',
+        'fifteen', 'sixteen', 'seventeen', 'eighteen' , 'nineteen'
+    ];
+
+    let tensMore = ['','', 'twenty', 'thirty', 'forty', 'fifty',
+        'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    let hundreds = ['', 'one hundred', 'two hundred', 'three hundred',
+        'four hundred', 'five hundred', 'six hundred',
+        'seven hundred', 'eight hundred', 'nine hundred'
+    ];
+
+    if (number === 0) return readableNumber = 'zero';
+
+    if (numberStringTemp.length === 3) {        
+        readableNumber += hundreds[numberStringTemp[0]];
+        numberStringTemp = numberStringTemp.slice(1)
+        if(numberStringTemp[0] == 0) numberStringTemp = numberStringTemp.slice(1);
+    }
+
+    if (numberStringTemp.length === 2 && numberStringTemp == 10) {        
+        readableNumber = readableNumber + ' ' + ten[numberStringTemp[0]];
+        numberStringTemp = '';
+    }
+
+    if (numberStringTemp.length === 2 && numberStringTemp >= 11 && numberStringTemp < 20) {
+        readableNumber = readableNumber + ' ' + tens[numberStringTemp[1]];
+        numberStringTemp = '';
+    }
+
+    if (numberStringTemp.length === 2 && numberStringTemp >= 20 && numberStringTemp < 100) {
+        readableNumber = readableNumber + ' ' + tensMore[numberStringTemp[0]];
+        numberStringTemp = numberStringTemp.slice(1);
+    }
+
+    if (numberStringTemp.length === 1 && numberStringTemp >= 1 && numberStringTemp < 10) {
+        readableNumber = readableNumber + ' ' + ones[numberStringTemp[0]];
+        numberStringTemp = '';
+    }
+
+    return readableNumber.trim();
 }
